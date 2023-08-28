2d2440c8e0d64a3c35c4b39e357c00d4dff261c3
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-08-28 17:41:43 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 5050;
+
+//Routes
+//const wordRoutes = require('./routes/wordsRoute');
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
+//app.use('/words', wordRoutes);
+
+app.listen(PORT, () => {
+	console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
